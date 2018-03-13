80116a22961c067556b7c397e91316180ae18311
RFS-0 <remo@mountain-yard.ch>
2018-03-13 18:59:29 +0100

    Setup basic project structure

---
+++ apirec/src/main/java/ase/rsse/app/App.java
@@ -0,0 +1,13 @@
+package ase.rsse.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
