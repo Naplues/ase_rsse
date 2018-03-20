a2a6ee4fd0e39e10fbefa3a61a0778c0c24f76db
RFS-0 <remo@mountain-yard.ch>
2018-03-21 00:33:54 +0100

    Add methods for reading zips

--- apirec/src/main/java/ase/rsse/utils/IoUtility.java
+++ apirec/src/main/java/ase/rsse/utils/IoUtility.java
@@ -1,18 +1,13 @@
 package ase.rsse.utils;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 import com.google.common.collect.Lists;
 
+import cc.kave.commons.model.events.IIDEEvent;
 import cc.kave.commons.model.events.completionevents.Context;
 import cc.kave.commons.utils.io.Directory;
 import cc.kave.commons.utils.io.IReadingArchive;
@@ -22,7 +17,7 @@ public class IoUtility {
 
 	public static Context readFirstContext(String dir) {
 		for (String zip : findAllZips(dir)) {
-			List<Context> ctxs = read(zip);
+			List<Context> ctxs = readContext(zip);
 			return ctxs.get(0);
 		}
 		return null;
@@ -32,17 +27,31 @@ public class IoUtility {
 		LinkedList<Context> res = Lists.newLinkedList();
 
 		for (String zip : findAllZips(dir)) {
-			res.addAll(read(zip));
+			res.addAll(readContext(zip));
 		}
 		return res;
 	}
 
-	public static List<Context> read(String zipFile) {
+	public static List<Context> readContext(String pathToContext) {
 		LinkedList<Context> res = Lists.newLinkedList();
 		try {
-			IReadingArchive ra = new ReadingArchive(new File(zipFile));
+			IReadingArchive ra = new ReadingArchive(new File(pathToContext));
 			while (ra.hasNext()) {
 				res.add(ra.getNext(Context.class));
+			}
+			ra.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	public static List<IIDEEvent> readEvent(String pathToEvent) {
+		LinkedList<IIDEEvent> res = Lists.newLinkedList();
+		try {
+			IReadingArchive ra = new ReadingArchive(new File(pathToEvent));
+			while (ra.hasNext()) {
+				res.add(ra.getNext(IIDEEvent.class));
 			}
 			ra.close();
 		} catch (Exception e) {
@@ -57,34 +66,5 @@ public class IoUtility {
 
 	public static Set<String> findAllJsons(String dir) {
 		return new Directory(dir).findFiles(s -> s.endsWith(".json"));
-	}
-
-	public static void unZip(String zipFile, int count) {
-		byte[] buffer = new byte[1024];
-
-		try {
-			ZipInputStream zis = new ZipInputStream(
-					new FileInputStream(Paths.get("Contexts-170503", zipFile).toFile()));
-			ZipEntry ze = zis.getNextEntry();
-			while (ze != null) {
-				String fileName = ze.getName();
-				File newFile = new File(Integer.toString(count) + "_" + fileName);
-				if (newFile.getParent() != null) {
-					new File(newFile.getParent()).mkdirs();
-				}
-				FileOutputStream fos = new FileOutputStream(newFile);
-				int len;
-				while ((len = zis.read(buffer)) > 0) {
-					fos.write(buffer, 0, len);
-				}
-
-				fos.close();
-				ze = zis.getNextEntry();
-			}
-			zis.closeEntry();
-			zis.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
