daa817516c91265f62d254ccab0b102a224dd597
KuersatAydinli <kuersat.aydinli@uzh.ch>
2018-03-25 17:39:51 +0200

    extracted completion events from 2.zip

--- apirec/src/main/java/ase/rsse/cookbook/Cook.java
+++ apirec/src/main/java/ase/rsse/cookbook/Cook.java
@@ -16,37 +16,53 @@ public class Cook {
 
 	public static void main(String[] args) {
 		// get all contexts for a given C# solution on GitHub
-		List<Context> contexts = IoUtility.readContext(
-				"F:\\Wifo_Msc\\2. Semester\\Advanced SW Eng\\ase_rsse\\apirec\\src\\main\\java\\ase\\rsse\\cookbook\\resharper-angularjs.sln-contexts.zip");
-		for (Context ctx : contexts) {
-			System.out.println(ctx);
-		}
+//		List<Context> contexts = IoUtility.readContext(
+//				"F:\\Wifo_Msc\\2. Semester\\Advanced SW Eng\\ase_rsse\\apirec\\src\\main\\java\\ase\\rsse\\cookbook\\resharper-angularjs.sln-contexts.zip");
+//		for (Context ctx : contexts) {
+//			System.out.println(ctx);
+//		}
 
 		// get all events of a given user
 		List<IIDEEvent> events = IoUtility
 				.readEvent("F:\\Wifo_Msc\\2. Semester\\Advanced SW Eng\\ase_rsse\\apirec\\src\\main\\java\\ase\\rsse\\cookbook\\2.zip");
+
+		System.out.println("########## Total number of Events: " + events.size()+" ##########");
+
+		ArrayList<VersionControlEvent> versionControlEvents = new ArrayList<>();
+		ArrayList<CompletionEvent> completionEvents = new ArrayList<>();
+
 		for (IIDEEvent event : events) {
-			ArrayList<VersionControlEvent> versionControlEvents = new ArrayList<>();
-			ArrayList<CompletionEvent> completionEvents = new ArrayList<>();
 
 			// get all the commits of the user
-			if (event instanceof VersionControlEvent) {
-				VersionControlEvent versionControlEvent = (VersionControlEvent) event;
-				versionControlEvents.add(versionControlEvent);
-			} else if (event instanceof CompletionEvent) {
-				CompletionEvent completionEvent = (CompletionEvent) event;
-				completionEvents.add(completionEvent);
+//			if (event instanceof VersionControlEvent) {
+//				VersionControlEvent versionControlEvent = (VersionControlEvent) event;
+//				versionControlEvents.add(versionControlEvent);
+//			} else if (event instanceof CompletionEvent) {
+//				CompletionEvent completionEvent = (CompletionEvent) event;
+//				completionEvents.add(completionEvent);
+//			}
+			if(event instanceof CompletionEvent){
+				completionEvents.add((CompletionEvent) event);
 			}
 
 			// order the events by date
-			List<VersionControlEvent> vceSorted = versionControlEvents.stream()
-					.filter(Objects::nonNull)
-					.sorted(Comparator.comparing(VersionControlEvent::getTriggeredAt, Comparator.reverseOrder()))
-					.collect(Collectors.toList());
-			List<CompletionEvent> ceSorted = completionEvents.stream()
-					.filter(Objects::nonNull)
-					.sorted(Comparator.comparing(CompletionEvent::getTriggeredAt, Comparator.reverseOrder()))
-					.collect(Collectors.toList());
+//			List<VersionControlEvent> vceSorted = versionControlEvents.stream()
+//					.filter(Objects::nonNull)
+//					.sorted(Comparator.comparing(VersionControlEvent::getTriggeredAt, Comparator.reverseOrder()))
+//					.collect(Collectors.toList());
 		}
+		System.out.println("########## Total number of Completion Events: " + completionEvents.size()+" ##########");
+		List<CompletionEvent> ceSorted = completionEvents.stream()
+				.filter(Objects::nonNull)
+				.sorted(Comparator.comparing(CompletionEvent::getTriggeredAt))
+				.collect(Collectors.toList());
+		System.out.println("########## Total number of sorted Completion Events: " + ceSorted.size()+" ##########");
+		for(CompletionEvent completionEvent:ceSorted){
+			System.out.println("Event Trigger at: " + completionEvent.getTriggeredAt());
+		}
+		System.out.println("########## First SST ##########");
+		System.out.println(ceSorted.get(0).getContext().getSST());
+		System.out.println("########## Filename of First SST ##########");
+		System.out.println(ceSorted.get(0).getContext().getTypeShape().getTypeHierarchy().getElement());
 	}
 }
