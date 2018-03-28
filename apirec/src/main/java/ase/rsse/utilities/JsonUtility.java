fb18928df51df6a4c9f56977ab9db2053e438b06
RFS-0 <remo@mountain-yard.ch>
2018-03-28 17:02:49 +0200

    Add Utility to handle JSON serialization

---
+++ apirec/src/main/java/ase/rsse/utilities/JsonUtility.java
@@ -0,0 +1,28 @@
+package ase.rsse.utilities;
+
+import com.google.gson.Gson;
+
+import ase.rsse.apirec.transactions.Transaction;
+
+public final class JsonUtility {
+
+	private static Gson _gson;
+
+	public static String JSON_POST_FIX = ".json";
+
+	private static Gson getGson() {
+		if (_gson == null) {
+			_gson = new Gson();
+		}
+		return _gson;
+	}
+
+	public static String toJson(Object obj) {
+		return getGson().toJsonTree(obj).toString();
+	}
+
+	public static Transaction fromJson(String json) {
+		Transaction ts = getGson().fromJson(json, Transaction.class);
+		return ts;
+	}
+}
