9f6e7da177ca154e123e4f963a95938e2c3fd013
RFS-0 <remo@mountain-yard.ch>
2018-05-23 14:26:38 +0200

    Add directory for test data

--- apirec/src/main/java/ase/rsse/apirec/transactions/ITransactionConstants.java
+++ apirec/src/main/java/ase/rsse/apirec/transactions/ITransactionConstants.java
@@ -3,5 +3,6 @@ package ase.rsse.apirec.transactions;
 public interface ITransactionConstants {
 	
 	public static final String TRANSACTION_DIRECTORY = "Transactions";
+	public static final String TEST_TRANSACTION_DIRECTORY = "TestTransactions";
 	public static final String EVENTS_DIRECTORY = "Events-170301-2";
 }
