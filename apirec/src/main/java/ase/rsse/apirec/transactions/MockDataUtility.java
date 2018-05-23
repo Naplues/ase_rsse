6d2a439a770243bca9e026ef89a14074848be754
RFS-0 <remo@mountain-yard.ch>
2018-05-23 14:34:56 +0200

    Add Apache License

--- apirec/src/main/java/ase/rsse/apirec/transactions/MockDataUtility.java
+++ apirec/src/main/java/ase/rsse/apirec/transactions/MockDataUtility.java
@@ -1,4 +1,22 @@
+/*Copyright [2018] [Kürsat Aydinli & Remo Schenker]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
 package ase.rsse.apirec.transactions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 import ase.rsse.apirec.transactions.changecontext.AtomicChange;
 import ase.rsse.apirec.transactions.changecontext.ChangeContext;
@@ -9,10 +27,6 @@ import ase.rsse.apirec.transactions.query.QueryCodeContext;
 import ase.rsse.apirec.transactions.query.QueryTransaction;
 import ase.rsse.utilities.IoUtility;
 import ase.rsse.utilities.JsonUtility;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
 
 public class MockDataUtility {
 	
