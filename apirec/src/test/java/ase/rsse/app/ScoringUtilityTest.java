139edad46523c49602fc7be15a99996ec92f4ba2
RFS-0 <remo@mountain-yard.ch>
2018-05-23 14:28:43 +0200

    Update scoring test

--- apirec/src/test/java/ase/rsse/app/ScoringUtilityTest.java
+++ apirec/src/test/java/ase/rsse/app/ScoringUtilityTest.java
@@ -2,22 +2,17 @@ package ase.rsse.app;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import ase.rsse.apirec.transactions.Transaction;
 import ase.rsse.apirec.transactions.changecontext.AtomicChange;
 import ase.rsse.utilities.IoUtility;
 import ase.rsse.utilities.ScoringUtility;
-import cc.kave.commons.model.events.completionevents.ICompletionEvent;
 
 public class ScoringUtilityTest {
 
@@ -29,30 +24,19 @@ public class ScoringUtilityTest {
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		IoUtility.deleteTransactionFile(MockDataUtility.TEST_QUERY_TRANSACTION);
+		IoUtility.deleteTestTransactionFile(MockDataUtility.TEST_QUERY_TRANSACTION + ".json");
 		for (String fileName: MockDataUtility.MOCK_TRANSACTIONS) {
-			IoUtility.deleteTransactionFile(fileName);
+			IoUtility.deleteTestTransactionFile(fileName + ".json");
 		}
 	}
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
-	@Ignore
 	public void testScoreChangeContext() {
-		ArrayList<Transaction> allTransactions = ScoringUtility.getAllTransactions();
-		Set<AtomicChange> allCandidateChanges = ScoringUtility.getAllCandidateChanges(MockDataUtility.QUERY_CHANGE_CONTEXT,	MockDataUtility.QUERY_CODE_CONTEXT);
+		ArrayList<Transaction> allTransactions = ScoringUtility.getAllTestTransactions();
+		Set<AtomicChange> allCandidateChanges = ScoringUtility.getAllTestCandidateChanges(MockDataUtility.QUERY_CHANGE_CONTEXT,	MockDataUtility.QUERY_CODE_CONTEXT);
 		
-//		Assert.assertEquals(5, allTransactions.size());
-//		Assert.assertEquals(5, allCandidateChanges.size());
-		Assert.assertEquals(54062, allTransactions.size());
-		Assert.assertEquals(71, allCandidateChanges.size());
+		Assert.assertEquals(5, allTransactions.size());
+		Assert.assertEquals(5, allCandidateChanges.size());
 		
 		HashMap<String, Double> changeContextScores = ScoringUtility.scoreChangeContext(allCandidateChanges, MockDataUtility.QUERY_CHANGE_CONTEXT);
 		
@@ -64,25 +48,19 @@ public class ScoringUtilityTest {
 	}
 	
 	@Test
-	@Ignore
 	public void testScoreCodeContext() {
-		ArrayList<Transaction> allTransactions = ScoringUtility.getAllTransactions();
-		Set<AtomicChange> allCandidateChanges = ScoringUtility.getAllCandidateChanges(MockDataUtility.QUERY_CHANGE_CONTEXT,	MockDataUtility.QUERY_CODE_CONTEXT);
+		ArrayList<Transaction> allTransactions = ScoringUtility.getAllTestTransactions();
+		Set<AtomicChange> allCandidateChanges = ScoringUtility.getAllTestCandidateChanges(MockDataUtility.QUERY_CHANGE_CONTEXT,	MockDataUtility.QUERY_CODE_CONTEXT);
 
 		Assert.assertEquals(5, allTransactions.size());
 		Assert.assertEquals(5, allCandidateChanges.size());
 		
-		HashMap<String,Double> codeContextScores = ScoringUtility.scoreCodeContext(allCandidateChanges, MockDataUtility.QUERY_CODE_CONTEXT);
+		HashMap<String,Double> codeContextScores = ScoringUtility.scoreTestCodeContext(allCandidateChanges, MockDataUtility.QUERY_CODE_CONTEXT);
 		
 		Assert.assertEquals(0.2, codeContextScores.get("add"), 0.01);
 		Assert.assertEquals(0.2, codeContextScores.get("contains"), 0.01);
 		Assert.assertEquals(0.2, codeContextScores.get("addAll"), 0.01);
 		Assert.assertEquals(0.2, codeContextScores.get("clear"), 0.01);
 		Assert.assertEquals(0.2, codeContextScores.get("remove"), 0.01);
-	}
-	
-	@Test
-	public void testScoring() {
-		List<ICompletionEvent> firstEvent = IoUtility.readEvent("XPRD.AnalyticSystemMap.AdministrationModel.Db.EFConfiguration.Location13.json");
 	}
 }
